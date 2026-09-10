38f157a6fd2904ac4495ccf236c393ba6ee7a1fa
Albin_Tymbark <jasiekhalota@gmail.com>
2026-09-10 12:29:17 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -15,6 +15,16 @@ const FILES = [
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
